e9c8716c7cafbfb26bd3e823791ae689634a1cfe
Wernher Behrendt <wernher.behrendt@gmail.com>
2024-11-07 19:55:49 +0100

    Adding GTW Logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import gtw_logo from './gtw_logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <img src={gtw_logo} className="GreenTwin-logo" alt="GreenTwin-logo" />
           <h1 className="App-title">Welcome to my own React (7. Nov. 2024)</h1>
         </header>
         <p className="App-intro">
